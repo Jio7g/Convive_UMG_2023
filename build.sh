06b59b91598a080073664a3ebe61094bd47d652f
Carlos Ramos <142697112+Jio7g@users.noreply.github.com>
2023-09-17 17:34:20 -0600

    Update build.sh
    delete script superuser Django

--- build.sh
+++ build.sh
@@ -9,5 +9,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Ejecuta el comando para crear un superusuario
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('ConviveAdmin', ' ', 'UMG_2023')" | python manage.py shell
+
